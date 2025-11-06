b1906eae0fd7090e744bfdc9de633921441d67d8
Achrafcodes <essoussygamer@gmail.com>
2025-11-06 13:45:26 +0100

    done

--- src/controllers/refresh.controlers.js
+++ src/controllers/refresh.controlers.js
@@ -1,6 +1,19 @@
+import { header } from 'express-validator';
 import jwt from 'jsonwebtoken';
-
+import User from '../model/users.model.js';
 export const refresh = async (req, res) => {
-  const { refreshtoken } = req.body;
-  console.log(req.body);
+  const { refreshToken } = req.body;
+  if (!refreshToken) {
+    return res.status(401).json({
+      message: 'no refresh token was found!',
+    });
+  }
+  const user = await User.findOne({ refreshToken: refreshToken });
+
+  const validToken = jwt.verify(refreshToken, proccess.env.REFRESH_TOKEN);
+  if (!validToken) return res.status(401).json({ message: 'invalid jwt refresh Token!' });
+  const newToken = jwt.sign({ id: user._id }, process.env.REFRESH_TOKEN, {
+    expiresIn: '15d',
+  });
+  User.refreshToken = newToken;
 };

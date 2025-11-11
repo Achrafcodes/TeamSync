8616edf392ef6d807e7ce615346a547ab54e4bd9
Achrafcodes <essoussygamer@gmail.com>
2025-11-11 13:35:32 +0100

    done

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,7 +3,7 @@ import { GetTaskFromId } from '../controllers/tasks/findTaskbyId.controller.js';
 import { AddNewTask } from '../controllers/tasks/addNewTask.controller.js';
 import { getTasks } from '../controllers/tasks/getTasks.controller.js';
 import { UpdateTask } from '../controllers/tasks/updateTask.controller.js';
-import { DeleteTask } from '../controllers/tasks/deleteTask.controler.js';
+import { DeleteTask } from '../controllers/tasks/deleteTask.controller.js';
 const router = Router();
 
 router.post('/add-task', AddNewTask);
